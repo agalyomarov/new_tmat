6b67ac38ba3037de5a29ae5c82086899835d754f
Agaly Omarov <97389793+agalyomarov@users.noreply.github.com>
2022-07-11 20:12:28 +0300

    yes

--- app/Http/Controllers/Admin/BalanceController.php
+++ app/Http/Controllers/Admin/BalanceController.php
@@ -7,6 +7,7 @@ use App\Models\BalanceHistory;
 use App\Models\Dealer;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
@@ -19,28 +20,36 @@ class BalanceController extends Controller
     }
     public function store(Request $request)
     {
-        $data = $request->all();
-        $filename = config('app.FILE_NAME');
-        if (Storage::disk('public')->exists($filename)) {
-            $balance = (float)Storage::disk('public')->get($filename);
-        } else {
-            Storage::disk('public')->put($filename, 0);
-            $balance = (float)Storage::disk('public')->get($filename);
+
+        try {
+            $data = $request->all();
+            $filename = config('app.FILE_NAME');
+            if (Storage::disk('public')->exists($filename)) {
+                $balance = (float)Storage::disk('public')->get($filename);
+            } else {
+                Storage::disk('public')->put($filename, 0);
+                $balance = (float)Storage::disk('public')->get($filename);
+            }
+            $validator = Validator::make($data, [
+                'login' => ['required', 'exists:dealers,login'],
+                'sum' => ['required', 'numeric', 'min:0.1', 'max:' . $balance],
+            ]);
+            if ($validator->fails()) {
+                return redirect()
+                    ->route('admin.balance.index')
+                    ->withErrors($validator)
+                    ->withInput();
+            }
+            $validated = $validator->validated();
+            DB::beginTransaction();
+            Dealer::where('login', $validated['login'])->increment('balance', $validated['sum']);
+            $balance = $balance - $validated['sum'];
+            Storage::disk('public')->put($filename, $balance);
+            DB::commit();
+            return redirect()->route('admin.dealer.index');
+        } catch (\Exception $e) {
+            DB::rollback();
+            return redirect()->route('admin.dealer.index');
         }
-        $validator = Validator::make($data, [
-            'login' => ['required', 'exists:dealers,login'],
-            'sum' => ['required', 'numeric', 'min:0.1', 'max:' . $balance],
-        ]);
-        if ($validator->fails()) {
-            return redirect()
-                ->route('admin.balance.index')
-                ->withErrors($validator)
-                ->withInput();
-        }
-        $validated = $validator->validated();
-        Dealer::where('login', $validated['login'])->increment('balance', $validated['sum']);
-        $balance = $balance - $validated['sum'];
-        Storage::disk('public')->put($filename, $balance);
-        return redirect()->route('admin.dealer.index');
     }
 }
